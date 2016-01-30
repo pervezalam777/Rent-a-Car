133f57adaf3c9abaaabd668d4dcb900b3318a7e6
gargi mukherjee <gargi mukherjee>
2016-01-30 11:20:29 +0530

    indentation for controller done

--- public/app/controllers/appCtrl.js
+++ public/app/controllers/appCtrl.js
@@ -2,79 +2,64 @@ angular.module('mainCtrl', [])
 
 .controller("MainController", function($scope,$location,$http){
 	$scope.rentaCar = function(){
-			$location.path('/find');
-			console.log("Rent a car is called");
-			$location.path('/find');			
-		};
+		$location.path('/find');
+		$location.path('/find');			
+	};
 
-		$scope.addCarforRent = function(){
-			$location.path("/rentmycar");
-		};
+	$scope.addCarforRent = function(){
+		$location.path("/rentmycar");
+	};
 
-		$scope.gotoCarList = function(){
-			var pincodeValue = 	$scope.pincode;
+	$scope.gotoCarList = function(){
+		var pincodeValue = 	$scope.pincode;
+		$scope.message = "";
 
-			$scope.message = "";
-
-			if(pincodeValue == " " || pincodeValue == null){
-				$scope.message = "Pincode is required";
-			}
-			else if(isNaN(pincodeValue)) {
-				$scope.message = "This is not a numbers";
-			}
-			else if (pincodeValue.length !== 6 )
-			{
-				$scope.message = "This is not a valid pincode, pincode should be of 6 digit";
-			}
-			else {
-				$scope.message = "Thanks for entering a valid pincode";
-				$location.path('/cars/' + $scope.pincode);
-		    }
+		if(pincodeValue == " " || pincodeValue == null){
+			$scope.message = "Pincode is required";
 		}
+		else if(isNaN(pincodeValue)) {
+			$scope.message = "This is not a numbers";
+		}
+		else if (pincodeValue.length !== 6 ){
+			$scope.message = "This is not a valid pincode, pincode should be of 6 digit";
+		}
+		else {
+			$scope.message = "Thanks for entering a valid pincode";
+			$location.path('/cars/' + $scope.pincode);
+		}
+	}
 })
 
 .controller("CarsController", function($scope,$location,$http,$routeParams){
-$scope.dataset = [];
-$http.get("../../app/models/cars.json")
-       .then(function(response){ 
-       	$scope.dataset = response.data; 
-        $scope.carFilter = $routeParams.param;
-        
+	$scope.dataset = [];
+	$http.get("../../app/models/cars.json")
+	.then(function(response){ 
+		$scope.dataset = response.data; 
+		$scope.carFilter = $routeParams.param;
+	})
+
+	$scope.rentCar= function(){ 
+		$scope.carId = event.target.id;
+		$location.path('/rentform/' + $scope.carId);
+	}
 })
 
-$scope.rentCar= function(){ 
-	$scope.carId = event.target.id;
-	$location.path('/rentform/' + $scope.carId);
-   }
- 
-})
+.controller("DealerController", function($scope,$location,$http){})
 
-.controller("DealerController", function($scope,$location,$http){
-})
-
-
-.controller("RentYourCarController", function($scope,$location,$http){
-})	
-
+.controller("RentYourCarController", function($scope,$location,$http){})	
 
 .controller("RentCarController", function($scope,$location,$http,$routeParams){
-$scope.carData = ""
-$http.get("../../app/models/" + $routeParams.param + ".json")
-       .then(function(response){ 
-       	$scope.carData = response.data; 
-        console.log($scope.carData);
-       })
-/*Validations for rent it form*/
+	$scope.carData = ""
+	$http.get("../../app/models/" + $routeParams.param + ".json")
+	.then(function(response){ 
+		$scope.carData = response.data; 
+	})
 
-$scope.validateMobileNumber = function(){
-	
- if($scope.userMobile = " " || isNaN($scope.userMobile)){
- 	$scope.validationMessage = "Please enter a 10 digit mobile number";
- 	console.log($scope.userMobile);
- 	
-  }	
-}
+/*Validations for rent it form-validation of mobile number*/
+	$scope.validateMobileNumber = function(){
 
-
-
+		if($scope.userMobile = " " || isNaN($scope.userMobile)){
+			$scope.validationMessage = "Please enter a 10 digit mobile number";
+		}	
+	}
 })
